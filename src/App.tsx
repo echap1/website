64c0fbd3ffe2c3937bf893f5932fc28c37e2b1b1
Ethan Chapman <ethan.chapman0@outlook.com>
2021-05-10 14:07:25 -0400

    Add socials

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Development Branch!
-        </p>
-      </header>
-    </div>
-  );
+    let message: string = "This website is a work in progress.\nHere are my socials in the meantime:"
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    {message}
+                </p>
+                <ul id="menu">
+                    <li><a href="https://github.com/echapman2022" className="fa fa-github"/></li>
+                    <li><a href="https://www.instagram.com/ethanmchapman/" className="fa fa-instagram"/></li>
+                    <li><a href="mailto:ethan.chapman0@outlook.com" className="fa fa-envelope"/></li>
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
